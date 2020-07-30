46ff58171e0b1bdf29e0ffc2f00fe3e45ae147e9
meivaldi <meivaldi@gmail.com>
2020-07-30 10:23:42 +0700

    Nothing

--- app/src/main/java/com/rumahdev/smartacn/MainActivity.java
+++ app/src/main/java/com/rumahdev/smartacn/MainActivity.java
@@ -2,39 +2,23 @@ package com.rumahdev.smartacn;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.HashMap;
-import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     LinearLayout button_hasil;
-    CarouselView carouselView;
-    int[] sampleImages = {R.drawable.slide_1, R.drawable.slide_2, R.drawable.slide_3, R.drawable.slide_4, R.drawable.slide_5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -168,18 +152,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        carouselView = findViewById(R.id.carouselView);
-        carouselView.setPageCount(sampleImages.length);
-        carouselView.setImageListener(imageListener);
-
     }
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImages[position]);
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        }
-    };
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
